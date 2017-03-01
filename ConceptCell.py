f62ed371ba4f5df40cca5346338bfd3199768f47
Eric <innovacious@hotmail.com>
2017-03-02 00:30:20 +0800

    Adapted concept cell to multiple dimensional input (not tested yet).

--- ConceptCell.py
+++ ConceptCell.py
@@ -13,24 +13,26 @@ def concept_cell():
     beta = 1.0
     theta = 1.0
 
-    x = tf.placeholder(tf.float32, shape=[1, None])  # TODO: type?
-    y_ = tf.placeholder(tf.float32, shape=[1, None])
-    c0 = tf.zeros([rbf_num, 1])
-    c = tf.Variable(tf.zeros([rbf_num, 1]))  # TODO: initialization strategy
-    r = tf.Variable(tf.ones([rbf_num, 1]))  # TODO: initialization strategy & division-by-zero
-    W = tf.ones([1, rbf_num])  # Could be variables
-    x = tf.tile(x, [rbf_num, 1])
+    x = tf.placeholder(tf.int32, shape=[None, 1, input_dimension])
+    y_ = tf.placeholder(tf.int32, shape=[None, 1])
+    c0 = tf.zeros([rbf_num, input_dimension])
+    c = tf.Variable(tf.zeros([rbf_num, input_dimension]))  # TODO: initialization strategy
+    r = tf.Variable(tf.ones([1, rbf_num]))  # TODO: initialization strategy & division-by-zero
+    w = tf.ones([rbf_num, 1])  # Could be variables
+    x = tf.tile(x, [1, rbf_num, 1])
 
-    rbfs = (x - c) / r          # Broadcasting feature. Cool!
+    rbfs = (x - c)          # Broadcasting feature. Cool!
     rbfs = tf.square(rbfs)
+    rbfs = tf.reduce_sum(rbfs, 2)
+    rbfs = tf.multiply(rbfs, tf.reciprocal(r))
     rbfs = -rbfs
     rbfs = tf.exp(rbfs)
 
-    y = tf.tanh(tf.matmul(W, rbfs))          # No bias? Only one output?
+    y = tf.tanh(tf.matmul(rbfs, w))          # No bias? Only one output?
 
     loss = tf.reduce_mean(tf.square(y - y_)) \
-           + alpha * tf.reduce_sum(beta * tf.sigmoid(tf.square(c0 - c))) \
-           + theta * tf.reduce_sum(tf.square(r))
+        + alpha * tf.reduce_sum(beta * tf.sigmoid(tf.square(c0 - c))) \
+        + theta * tf.reduce_sum(tf.square(r))
     train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
     return y, loss, train_step
 
